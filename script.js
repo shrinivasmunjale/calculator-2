865d92dbe51bba14a655535d2b1f95884cc7554a
Shrinivas Munjale <shrinivasmunjale3@gmail.com>
2026-01-20 20:27:39 +0530

    Add files via upload
    solving the problem which is occur after culculating the opration afrer equal button press

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const result = document.querySelector(".result");
 const nums = document.querySelectorAll(".num");
 const equal = document.querySelector(".equal");
 const clear = document.querySelector("#clear");
+const op =document.querySelectorAll(".op");
 
 let currentInput="";
 
@@ -15,12 +16,15 @@ nums.forEach(num =>{
 });
 equal.addEventListener("click",()=>{
     result.innerHTML= eval(currentInput);
+    currentInput = result.innerHTML;
+
     
 })
 clear.addEventListener("click",()=>{
     result.innerHTML="0";
     currentInput="";
 })
+
 
 
 
